fdacc27350928670f636b0aaa3c767e564f8755b
arshad786pasha <arshad785pasha65@gmail.com>
2024-09-23 07:19:34 +0530

    ok

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
 import yt_dlp
 
-app = Flask(__name__)
+url = input('Enter the link')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Options for downloading
+ydl_opts = {
+    'format': 'best',
+    'outtmpl': '%(title)s.%(ext)s',  # This will save the video using its title
+}
 
-@app.route('/download', methods=['POST'])
-def download_video():
-    # Get the YouTube URL from the form
-    video_url = request.form['video_url']
-
-    if video_url:
-        # Options for downloading
-        ydl_opts = {
-            'format': 'best',
-            'outtmpl': '%(title)s.%(ext)s',  # Save the video using its title
-        }
-
-        try:
-            # Use yt_dlp to download the video
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([video_url])
-            return "Download complete!"
-        except Exception as e:
-            return f"An error occurred: {e}"
-
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
+try:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    print("Download complete!")
+except Exception as e:
+    print(f"An error occurred: {e}")
